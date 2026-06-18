3ddae56c90880a6245c5c5a53448c1ceced5e41b
abhi1-ironman <abhishek50101144@gmail.com>
2026-06-18 07:32:36 +0530

    my 17th day

---
+++ day 017/merge_array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,i,arr1[100],arr2[100],arr3[200];
+    printf("enter the size of first array ");
+    scanf("%d",&n1);
+    printf("enter the elements ");
+    for(i=0;i<n1;i++)
+    {
+        scanf("%d",&arr1[i]);
+    }
+    printf("enter the size of second array ");
+    scanf("%d",&n2);
+    printf("enter the elements ");
+    for(i=0;i<n2;i++)
+    {
+        scanf("%d",&arr2[i]);
+    }
+    for(i=0;i<n1;i++)
+    {
+        arr3[i]=arr1[i];
+    }
+    for(i=0;i<n2;i++)
+    {
+        arr3[n1+i]=arr2[i];
+    }
+    printf("the merged array will be ");
+    for(i = 0; i < n1 + n2; i++)
+    {
+        printf("%d ", arr3[i]);
+    }
+    return 0;
+}
